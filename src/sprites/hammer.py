f0490f225f11e6b5fcd7323db1ff009651d4c2b4
Uli <admin@deruli.de>
2023-11-06 23:44:57 +0100

    Hammer eingefügt

--- src/sprites/hammer.py
+++ src/sprites/hammer.py
@@ -1,10 +1,10 @@
-""" Chainsaw sprite """
+""" Hammer sprite """
 
 from sprites.inlinesprite import InlineSprite
 from sprites.takeable import Takeable
 
 class Hammer(Takeable, InlineSprite):
-    """ Chainsaw sprite class """
+    """ Hammer sprite class """
 
     def __init__(self, sprite_dir, cache, sprite='hammer.png'):
         """ Constructor """
