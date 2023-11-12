17a8ca5d3745d07322c94647bdae02e39c41bf21
Uli <admin@deruli.de>
2023-11-12 23:39:58 +0100

    Unnötiges Fill weg

--- src/components/fadeable_component.py
+++ src/components/fadeable_component.py
@@ -37,6 +37,5 @@ class FadeableComponent(Component):
         self.fadeout()
         pygame.mixer.music.fadeout(1000)
         while self.do_fade:
-            self.screen.fill((0, 0, 0))
             self.update_screen(self.screen)
             pygame.display.flip()
