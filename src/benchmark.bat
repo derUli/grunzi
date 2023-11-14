4198b4133a70d0519953697b516ce83090e160e8
Uli <admin@deruli.de>
2023-11-14 08:48:12 +0100

    Benchmark

--- src/benchmark.bat
+++ src/benchmark.bat
@@ -1,6 +1,4 @@
 @echo off
-
-game.py --benchmark 30 >> benchmark.txt
-game.py --benchmark 60 >> benchmark.txt
-game.py --benchmark 120 >> benchmark.txt
-game.py --benchmark 180 >> benchmark.txt
+Game.py --benchmark 60 >> benchmark.txt
+Game.py --benchmark 120 >> benchmark.txt
+Game.py --benchmark 240 >> benchmark.txt
