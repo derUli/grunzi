ff56a533ebce1594d4bd564d3e4ef143a801c251
Uli <admin@deruli.de>
2023-11-16 00:03:28 +0100

    Telescope Logik

--- src/sprites/coin.py
+++ src/sprites/coin.py
@@ -1,9 +1,10 @@
 """ Wall sprite """
 
 from sprites.takeable import Takeable
+from sprites.inlinesprite import InlineSprite
 
 
-class Coin(Takeable):
+class Coin(Takeable, InlineSprite):
     """ Takeable sprite class """
 
     def __init__(self, sprite_dir, cache, sprite='coin.png'):
---
+++ src/sprites/telescope.py
@@ -0,0 +1,29 @@
+""" Guitar sprite """
+from sprites.coin import Coin
+from sprites.sprite import Sprite
+
+
+class Telescope(Sprite):
+    """ Guitar sprite class """
+
+    def __init__(self, sprite_dir, cache, sprite=None):
+        """ Constructor """
+        super().__init__(sprite_dir, cache, 'telescope.png')
+        self.center_camera = False
+        self.sound = None
+
+    def handle_interact(self, element):
+        if element and element.state:
+            element.state.say(_('[Please insert coin]'))
+
+
+    def handle_interact_item(self, element):
+        # Activate the telescope with a coin
+        if not element:
+            return
+
+        if isinstance(element.state.inventory, Coin):
+            element.state.say(_('Not implemented yet'))
+            element.state.use_item = False
+            #element.state.inventory = None
+
