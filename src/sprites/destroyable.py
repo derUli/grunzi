48def91e4a6f2d059a990941f6b124b03bab8a9b
Uli <admin@deruli.de>
2023-11-16 15:39:12 +0100

    Destroyable

--- src/sprites/destroyable.py
+++ src/sprites/destroyable.py
@@ -1,10 +1,12 @@
 """ Destroyable sprite """
 import logging
-
+import pygame
 from sprites.chainsaw import Chainsaw
 from sprites.wall import Wall
 import random
 from utils.quality import pixel_fades_enabled
+from threading import Thread
+
 RUMBLE_CHAINSAW_DURATION = 300
 RUMBLE_CHAINSAW_HIGH_FREQUENCY = 1
 RUMBLE_CHAINSAW_LOW_FREQUENCY = 0
@@ -16,30 +18,29 @@ class Destroyable(Wall):
     def __init__(self, sprite_dir, cache, sprite='box.png'):
         """ Constructor """
         super().__init__(sprite_dir, cache, sprite)
-
+        self.persistent_pixels = 0
         self.fadeout = False
 
     def draw(self, screen, x, y):
         super().draw(screen, x, y)
-
-        if self.fadeout and not self.remove_random_pixel():
-            self.stop_fade()
 
     def handle_interact_item(self, element):
         # Destroy if player has the chainsaw
         if not element:
             return
 
-        if isinstance(element.state.inventory, Chainsaw) and not self.walkable:
+        if isinstance(element.state.inventory, Chainsaw) and not self.walkable and not self.fadeout:
             if element.state.inventory.attributes['fuel'] <= 0:
                 return
 
             if pixel_fades_enabled():
-                self.start_fade()
+                if not self.fadeout:
+                    self.start_fade()
             else:
                 self.purge = True
+                self.walkable = True
 
-            logging.debug('Destroyable sprite destroyed with chainsaw')
+                logging.debug('Destroyable sprite destroyed with chainsaw')
 
             element.state.inventory.play_sound()
 
@@ -52,31 +53,54 @@ class Destroyable(Wall):
                 )
 
     def start_fade(self):
-        self.fadeout = True
-        self.walkable = True
         self.sprite = self.sprite.copy().convert_alpha()
+        self.persistent_pixels = self.count_persistent_pixels()
+        self.fadeout = True
 
-    def stop_fade(self):
+        thread = Thread(target=self.remove_pixels)
+        thread.start()
+
+
+    def stop_fade(self, purge = False):
         self.fadeout = False
-        self.purge = True
+        self.purge = purge
+        logging.debug('Destroyable sprite destroyed with chainsaw')
+
+    def remove_pixels(self):
+        bulk = 5
+        while self.fadeout:
+            pygame.time.delay(1)
+            for i in range(bulk):
+                self.remove_random_pixel()
 
     def remove_random_pixel(self):
-        bulk = self.sprite.get_height() * 2
-        for i in range(bulk):
-            w, h = self.sprite.get_size()
+        w, h = self.sprite.get_size()
 
+        if self.persistent_pixels <= 0:
+            self.stop_fade(purge=True)
+            return
+
+        r,g,b,a = 0, 0, 0, 0
+        rand_x = 0
+        rand_y = 0
+
+        while a == 0:
             rand_x = random.randint(0, w - 1)
             rand_y = random.randint(0, h - 1)
 
             r, g, b, a = self.sprite.get_at((rand_x, rand_y))
 
-            if a != 0:
-                self.sprite.set_at((rand_x, rand_y), (r, g, b, 0))
+        self.sprite.set_at((rand_x, rand_y), (r, g, b, 0))
+        self.persistent_pixels -= 1
 
+
+    def count_persistent_pixels(self):
+        persistent = 0
+        w, h = self.sprite.get_size()
         for x in range(0, w):
             for y in range(0, h):
                 r, g, b, a = self.sprite.get_at((x, y))
                 if a > 0:
-                    return True
+                    persistent += 1
 
-        return False
+        return persistent
