df23dbe654da84fa3de4c3b16bd8a56f18e1009c
Uli <admin@deruli.de>
2023-11-18 21:08:35 +0100

    tests

--- tests/test_utils_string.py
+++ tests/test_utils_string.py
@@ -1,8 +1,8 @@
 import unittest
 
-from utils.string import natural_keys
+from utils.string import natural_keys, label_value
 class StringTest(unittest.TestCase):
-    def test_direction(self):
+    def test_natural_keys(self):
         arr = [
             '1',
             '001',
@@ -12,3 +12,11 @@ class StringTest(unittest.TestCase):
 
         self.assertEqual(['00', '001', '01', '1'], sorted(arr))
         self.assertEqual(['00', '1', '001', '01'], sorted(arr, key=natural_keys))
+
+    def test_label_value(self):
+        self.assertEqual(
+            'Level: 123.45',
+            label_value(
+                'Level',
+                123.45)
+        )
