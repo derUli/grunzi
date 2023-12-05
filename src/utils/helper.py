893abfd23fb3af6ef6ae06a05a22648654d11317
Uli <admin@deruli.de>
2023-12-05 22:20:23 +0100

    comments

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -5,6 +5,7 @@ from utils.path import get_userdata_path, is_windows
 
 
 def configure_logger(log_level):
+    """ Configure logger """
     log_file = os.path.join(get_userdata_path(), 'debug.log')
     logging.basicConfig(
         level=log_level,
