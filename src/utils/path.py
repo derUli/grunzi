988d625f02fa922a9fc3883ffa8e9c5a87bc0ba0
Uli <admin@deruli.de>
2023-12-15 23:48:12 +0100

    Kommentare§

--- src/utils/path.py
+++ src/utils/path.py
@@ -4,10 +4,12 @@ from constants.game import GAMEDIR_NAME_WINDOWS, GAMEDIR_NAME_LINUX
 
 
 def is_windows():
+    """ Check if we are on Windows """
     return os.name == 'nt'
 
 
 def get_userdata_path():
+    """ Get savegame dir """
     homedir = os.path.expanduser('~')
 
     if is_windows():
