5cec88443605b6ece9c291673cc6b11073d7fab8
Uli <admin@deruli.de>
2024-01-08 22:27:45 +0100

    Blut

--- src/sprites/horse.py
+++ src/sprites/horse.py
@@ -8,6 +8,8 @@ import time
 import pygame
 
 from constants.direction import DIRECTION_LEFT, DIRECTION_RIGHT, DIRECTION_UP, DIRECTION_DOWN
+from constants.headup import NPC_HEALTH_COLOR_FRIENDLY, NPC_HEALTH_HEIGHT
+from constants.graphics import SPRITE_SIZE
 from sprites.chainsaw import Chainsaw
 from sprites.character import Character
 from sprites.feather import Feather
@@ -31,13 +33,43 @@ class Horse(Killable, Character):
         """ Constructor """
         super().__init__(sprite_dir, cache, sprite)
         self.sentence = -1
-        
+        self.attributes = {
+            'blood': 0,
+            'given_blood': 0
+        }
+
         self.sentences = [
             _('Infinity bloodless!'),
             _('I don\'t have a single drop of blood left.'),
             _('Please give me some blood!'),
             _('I am infinitely thirsty for blood.')
         ]
+
+    
+    def draw(self, screen, x, y):
+        super().draw(screen, x, y)
+
+
+        pos = self.calculate_pos(x, y)
+
+        self.draw_health(screen, pos)
+    
+    def draw_health(self, screen, pos):
+
+        x, y = pos
+        w, h = SPRITE_SIZE
+
+        y += h - NPC_HEALTH_HEIGHT
+        
+
+        w = w / 100 * self.attributes['blood']
+        h = NPC_HEALTH_HEIGHT
+
+        surface = pygame.surface.Surface((w, h))
+
+        surface.fill(NPC_HEALTH_COLOR_FRIENDLY)
+
+        screen.blit(surface, (x, y))
 
     def next_sentence(self):
         self.sentence += 1
@@ -55,3 +87,21 @@ class Horse(Killable, Character):
                 return
 
             element.state.say(self.next_sentence())
+            
+
+    def ai(self, level):
+        part = 0.5
+
+
+        if round(self.attributes['given_blood']) <=  0 :
+            return
+        
+        self.attributes['blood'] += part
+        self.attributes['given_blood'] -= part
+
+        if self.attributes['blood'] > 100:
+            self.attributes['blood'] = 100
+            self.attributes['given_blood'] = 0
+        
+        if self.attributes['blood'] < 0:
+            self.attributes['blood'] = 0
