42340cbae833554da6927934a15d99db338da6b5
Uli <admin@deruli.de>
2024-01-13 15:31:22 +0100

    Code formatiert

--- src/state/task.py
+++ src/state/task.py
@@ -1,19 +1,32 @@
-""" Task display for pause menu """
+""" Current task """
+
 from typing import Union
 
 
 class Task:
     def __init__(self, task_id: Union[str, None]):
+        """
+        @param task_id: Identifier of the task
+        """
         self._task_id = task_id
 
-    def get_id(self):
+    def get_id(self) -> Union[str, None]:
+        """
+        @return: task id
+        """
         return self._task_id
 
-    def set_id(self, task_id: Union[str, None]):
+    def set_id(self, task_id: Union[str, None]) -> None:
+        """
+        Set task id
+        @param task_id: the task id
+        """
         self._task_id = task_id
 
-    def get_display_text(self):
-        """ Get the current task title """
+    def get_display_text(self) -> Union[str, None]:
+        """
+        @return: Display text for the current task
+        """
         task = _('Keine Aufgabe')
 
         tasks = {
@@ -26,4 +39,3 @@ class Task:
             task = tasks[self._task_id]
 
         return task
-
