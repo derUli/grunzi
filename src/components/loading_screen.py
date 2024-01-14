66e6cf2fdf28fa7387464aa61d10662913c42127
Uli <admin@deruli.de>
2024-01-14 02:52:35 +0100

    Progressbar

--- src/components/loading_screen.py
+++ src/components/loading_screen.py
@@ -2,14 +2,15 @@
 import pygame
 
 from components.component import Component
-from constants.headup import BOTTOM_UI_BACKGROUND
+from constants.headup import BOTTOM_UI_BACKGROUND, UI_MARGIN
 from utils.quality import font_antialiasing_enabled
 
+LOADING_COLOR = (255, 255, 255)
 
 class LoadingScreen(Component):
     """ Loading screen component """
 
-    def loading_screen(self, percentage=None, loading_text=None):
+    def loading_screen(self, val=None, loading_text=None):
         """
         :param int percentage Loading percentage
         :param string Loading Text
@@ -21,8 +22,8 @@ class LoadingScreen(Component):
             loading_text = _('Loading...')
 
         # Show percentage if given
-        if percentage is not None:
-            percentage = str(int(percentage)) + "%"
+        if val is not None:
+            percentage = str(int(val)) + "%"
             percentage = percentage.rjust(4, ' ')
 
             loading_text = ' '.join([loading_text, str(percentage)])
@@ -31,7 +32,7 @@ class LoadingScreen(Component):
         rendered_text = self.monotype_font.render(
             loading_text,
             font_antialiasing_enabled(),
-            (255, 255, 255)
+            LOADING_COLOR
         )
 
         # Calculate screen center
@@ -42,6 +43,22 @@ class LoadingScreen(Component):
         pos_y -= rendered_text.get_height() / 2
         # draw text on screen
         self.screen.blit(rendered_text, (pos_x, pos_y))
+
+        w = self.screen.get_width()
+        h = rendered_text.get_height()
+        onepercent_width = w / 100
+
+        pos_x = 0
+        pos_y = self.screen.get_height() - h
+
+        current_width = onepercent_width * val
+
+        if current_width > 0:
+            surf = pygame.surface.Surface((current_width, h))
+
+            surf.fill(LOADING_COLOR)
+            self.screen.blit(surf, (pos_x, pos_y))
+
 
         # Pump event queue and flip display to keep the application alive
         pygame.event.pump()
