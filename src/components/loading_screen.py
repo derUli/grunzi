283fa3c62528dbad50d8bd73e53aab47dd15bb13
Uli <admin@deruli.de>
2024-01-14 03:18:25 +0100

    Neuer Ladebildschirm

--- src/components/loading_screen.py
+++ src/components/loading_screen.py
@@ -1,11 +1,15 @@
 """ Loading Screen """
+import os
+
 import pygame
 
 from components.component import Component
 from constants.headup import BOTTOM_UI_BACKGROUND, UI_MARGIN
 from utils.quality import font_antialiasing_enabled
 
-LOADING_COLOR = (255, 255, 255)
+LOADING_BAR_COLOR = (255, 255, 255)
+LOADING_TEXT_COLOR = (255, 232, 238)
+
 
 class LoadingScreen(Component):
     """ Loading screen component """
@@ -32,20 +36,31 @@ class LoadingScreen(Component):
         rendered_text = self.monotype_font.render(
             loading_text,
             font_antialiasing_enabled(),
-            LOADING_COLOR
+            LOADING_TEXT_COLOR
         )
 
-        # Calculate screen center
-        pos_x, pos_y = self.screen.get_size()
-        pos_x = pos_x / 2
-        pos_y = pos_x / 2
-        pos_x -= rendered_text.get_width() / 2
-        pos_y -= rendered_text.get_height() / 2
-        # draw text on screen
-        self.screen.blit(rendered_text, (pos_x, pos_y))
+        backdrop = self.image_cache.load_image(
+            os.path.join(
+                self.data_dir,
+                'images',
+                'ui',
+                'loading.jpg'
+            ),
+            self.screen.get_size()
+        )
+
+        h = rendered_text.get_height() + UI_MARGIN
+
+        self.screen.blit(
+            backdrop,
+            (0, h),
+            (0, 0,
+             self.screen.get_width(),
+             self.screen.get_height() - (h * 2)
+             )
+        )
 
         w = self.screen.get_width()
-        h = rendered_text.get_height()
         onepercent_width = w / 100
 
         pos_x = 0
@@ -56,9 +71,17 @@ class LoadingScreen(Component):
         if current_width > 0:
             surf = pygame.surface.Surface((current_width, h))
 
-            surf.fill(LOADING_COLOR)
+            surf.fill(LOADING_BAR_COLOR)
             self.screen.blit(surf, (pos_x, pos_y))
 
+        # Calculate screen center
+        pos_x, pos_y = self.screen.get_size()
+        pos_x = pos_x / 2
+        pos_x -= rendered_text.get_width() / 2
+        pos_y = UI_MARGIN / 2
+
+        # draw text on screen
+        self.screen.blit(rendered_text, (pos_x, pos_y))
 
         # Pump event queue and flip display to keep the application alive
         pygame.event.pump()
