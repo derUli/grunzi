40246fb040d58b539694178cf24955b1848009ab
Uli <admin@deruli.de>
2024-01-14 15:05:55 +0100

    Food class

--- src/sprites/apple.py
+++ src/sprites/apple.py
@@ -1,32 +1,14 @@
 """ Apple sprite class """
-import os
 
-import sprites.sprite
-from utils.audio import play_sound
+from sprites.food import Food
 
+NUTRITIONAL_VALUE = 30
 
-class Apple(sprites.sprite.Sprite):
+class Apple(Food):
     """ Apple sprite class """
 
     def __init__(self, sprite_dir, cache, sprite=None):
         """ Constructor """
         super().__init__(sprite_dir, cache, 'apple.png')
 
-    def handle_interact(self, element):
-        """ Apple increases health """
-        if element and element.state and element.state.health < 100:
-            element.state.partial_heal(30)
-            self.purge = True
-            self.walkable = True
-
-            sound = os.path.abspath(
-                os.path.join(
-                    self.sprite_dir,
-                    '..',
-                    '..',
-                    'sounds',
-                    'pig',
-                    'smacks.ogg')
-            )
-
-            play_sound(sound)
+        self.nutritional_value = NUTRITIONAL_VALUE
