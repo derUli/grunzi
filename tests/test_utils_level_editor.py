d70b90b1eef50d0bc1e798585794c00a0485c496
Uli <admin@deruli.de>
2024-01-18 19:04:20 +0100

    tests

--- tests/test_utils_level_editor.py
+++ tests/test_utils_level_editor.py
@@ -3,7 +3,9 @@ import unittest
 
 import pygame
 
+import utils.quality
 from sprites.sprite import Sprite
+from state.settingsstate import SettingsState
 from utils.image import ImageCache
 from utils.level_editor import get_editor_blocks
 
@@ -12,12 +14,17 @@ class LevelEditorTest(unittest.TestCase):
     def setUp(self):
         pygame.init()
         pygame.display.set_mode((1280, 720))
+        utils.quality.settings_state = SettingsState()
 
     def tearDown(self):
         pygame.quit()
+        utils.quality.settings_state = None
 
     def test_get_level_editor(self):
-        sprites_dir = os.path.join(os.path.dirname(__file__), '..', 'data', 'sprites')
+        sprites_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..', 'src', 'data', 'images', 'sprites')
+        )
+        print(sprites_dir)
         cache = ImageCache()
         blocks = get_editor_blocks(sprites_dir, cache)
 
