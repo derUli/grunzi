a5c69cfc6943170aa54b734994f96bdc6f1c9070
Uli <admin@deruli.de>
2024-01-19 01:18:38 +0100

    Satz mit Pizza

--- src/sprites/wildpig.py
+++ src/sprites/wildpig.py
@@ -9,7 +9,20 @@ class Wildpig(Character):
     def __init__(self, sprite_dir, cache, sprite='dog.png'):
         """ Constructor """
         super().__init__(sprite_dir, cache, sprite)
+        self.sentence = 0
+        self.sentences = [
+            _('I am very wild.'),
+            _('Can I haz pizza?')
+        ]
 
     def handle_interact(self, element):
-        element.state.say(_('I am very wild.'))
+        if element.state.display_text.is_visible():
+            return
+
+        element.state.say(self.sentences[self.sentence])
         element.play_sound(PIG_SOUND_NOTHING)
+
+        if self.sentence < len(self.sentences) - 1:
+            self.sentence += 1
+
+
