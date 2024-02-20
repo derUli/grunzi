b1a1b9c4857ebc92dded2481e1b73c2e610693fb
Uli <admin@deruli.de>
2024-02-20 16:13:50 +0100

    Achievement

--- src/components/game/achievements.py
+++ src/components/game/achievements.py
@@ -5,10 +5,13 @@ import pygame
 
 import utils.quality
 from components.mixins.filmgrain import FilmGrain
+from components.settings.controls import HORIZONTAL_MARGIN, FONT_SIZE, LINE_LARGE_MARGIN
 from constants import gamepad
 from constants import keyboard
 from constants.game import REGULAR_FONT, LARGE_FONT_SIZE
 from state.achievements import AchievementsState
+
+TEXT_COLOR = (255, 255, 255)
 
 
 class Achievements(FilmGrain):
@@ -27,9 +30,13 @@ class Achievements(FilmGrain):
             os.path.join(data_dir, 'fonts', REGULAR_FONT),
             LARGE_FONT_SIZE)
 
-        self.rendered_text = None
-
         self.backdrop = None
+        fontfile = os.path.join(data_dir, 'fonts', REGULAR_FONT)
+        self.font = pygame.font.Font(
+            fontfile,
+            FONT_SIZE
+        )
+
         self.state = AchievementsState()
 
     def mount(self):
@@ -47,22 +54,47 @@ class Achievements(FilmGrain):
             self.backdrop = self.image_cache.load_image(
                 file, screen.get_size())
 
-        self.screen.blit(self.backdrop, (0, 0))
+        screen.blit(self.backdrop, (0, 0))
 
-        if not self.rendered_text:
-            self.rendered_text = self.regular_font.render(
-                _('Coming Soon'),
+        x = HORIZONTAL_MARGIN
+        y = HORIZONTAL_MARGIN
+
+        rendered_text = self.font.render(
+            _('Errungenschaften'),
+            utils.quality.font_antialiasing_enabled(),
+            pygame.Color(TEXT_COLOR)
+        )
+
+        screen.blit(rendered_text, (x, y))
+
+        y += rendered_text.get_height()
+        y += LINE_LARGE_MARGIN
+
+        for achievement in self.state.achievements:
+            rendered_text = self.font.render(
+                self.state.achievements[achievement].get_display_text(),
                 utils.quality.font_antialiasing_enabled(),
-                (255, 255, 255)
+                pygame.Color(TEXT_COLOR)
             )
 
-        pos_x, pos_y = self.screen.get_size()
-        pos_x = pos_x / 2
-        pos_y = pos_x / 2
-        pos_x -= self.rendered_text.get_width() / 2
-        pos_y -= self.rendered_text.get_height() / 2
+            screen.blit(rendered_text, (x, y))
 
-        screen.blit(self.rendered_text, (pos_x, pos_y))
+            x = screen.get_width() - HORIZONTAL_MARGIN
+
+            file = os.path.join(self.data_dir, 'images', 'ui', 'check.png')
+
+            image = self.image_cache.load_image(
+                file, (rendered_text.get_height(), rendered_text.get_height())
+            )
+
+            if not self.state.achievements[achievement].completed:
+                image = pygame.transform.grayscale(image)
+
+            screen.blit(image, (x, y))
+
+            x = HORIZONTAL_MARGIN
+            y += rendered_text.get_height()
+            y += LINE_LARGE_MARGIN
 
         self.draw_filmgrain(screen)
 
