d9ff4a5c5caf0319d66bb2f86b4efd1a28dd54e3
Uli <admin@deruli.de>
2024-02-24 15:03:52 +0100

    chase

--- src/sprites/characters/skullsprite.py
+++ src/sprites/characters/skullsprite.py
@@ -15,6 +15,7 @@ PLAYER_DAMPING = 0.2
 SIGHT_DISTANCE = 500
 SIGHT_CHECK_RESOLUTION = 2
 
+
 class SkullSprite(arcade.sprite.Sprite):
     def __init__(
             self,
@@ -22,7 +23,7 @@ class SkullSprite(arcade.sprite.Sprite):
             center_x=0,
             center_y=0
     ):
-        super().__init__(center_x=center_x, center_y = center_y)
+        super().__init__(center_x=center_x, center_y=center_y)
 
         self.move_force = PLAYER_MOVE_FORCE
         self.damping = PLAYER_DAMPING
@@ -42,7 +43,7 @@ class SkullSprite(arcade.sprite.Sprite):
 
         self.face = DEFAULT_FACE
         self.textures = None
-
+        self.astar_barrier_list = None
         self.update_texture()
 
     def update_texture(self):
@@ -53,7 +54,7 @@ class SkullSprite(arcade.sprite.Sprite):
 
         self.texture = self.textures[self.face - 1]
 
-    def update(self, player = None, walls = None):
+    def update(self, player=None, walls=None):
 
         # Figure out if we should face left or right
         if self.change_x < 0:
@@ -66,19 +67,38 @@ class SkullSprite(arcade.sprite.Sprite):
         if not player or not walls:
             return
 
-        self.chasing = arcade.has_line_of_sight(player.position,
-            self.position,
-            walls=walls,
-            check_resolution=SIGHT_CHECK_RESOLUTION,
-            max_distance= SIGHT_DISTANCE
-        )
+        grid_size = self.texture.width * self._scale
 
-        astar_barrier_list = arcade.AStarBarrierList(walls)
-
-
-        print(arcade.astar_calculate_path(self.position,
-                                    player.position,
-                                    astar_barrier_list,
-                                    diagonal_movement=False))
-
+        # Calculate the playing field size. We can't generate paths outside of
+        # this.
+        playing_field_left_boundary = -grid_size * 2
+        playing_field_right_boundary = grid_size * 35
+        playing_field_top_boundary = grid_size * 17
+        playing_field_bottom_boundary = -grid_size * 2
         self.update_texture()
+
+        if arcade.has_line_of_sight(player.position,
+                                 self.position,
+                                 walls=walls,
+                                 check_resolution=SIGHT_CHECK_RESOLUTION,
+                                 max_distance=SIGHT_DISTANCE
+                                 ):
+            self.chasing = player
+
+        if self.chasing:
+            if not self.astar_barrier_list:
+                self.astar_barrier_list = arcade.AStarBarrierList(
+                    moving_sprite=self,
+                    blocking_sprites=walls,
+                    grid_size=grid_size,
+                    left=int(playing_field_left_boundary),
+                    right=playing_field_right_boundary,
+                    bottom=int(playing_field_bottom_boundary),
+                    top=playing_field_top_boundary
+                )
+
+            arcade.astar_calculate_path(self.position,
+                                              player.position,
+                                              self.astar_barrier_list,
+                                              diagonal_movement=False)
+
