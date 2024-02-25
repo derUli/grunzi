ba4ebbf464f611d1ed4fa14b3a10dfad014d7627
Uli <admin@deruli.de>
2024-02-25 20:32:09 +0100

    performance

--- src/sprites/characters/sprite.py
+++ src/sprites/characters/sprite.py
@@ -49,5 +49,5 @@ class Sprite(arcade.sprite.Sprite):
     def draw_debug(self):
         return
 
-    def update(self, player=None, walls=None, scene=None, physics_engine=None):
+    def update(self, player=None, scene=None, physics_engine=None):
         return
