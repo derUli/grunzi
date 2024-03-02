987757f0ddad554dbeb244b9680a7d38ac62031e
Uli <admin@deruli.de>
2024-03-02 02:51:47 +0100

    Add get_Ip, some visual chanfges

--- src/utils/netcode.py
+++ src/utils/netcode.py
@@ -19,10 +19,27 @@ def get_own_ip():
 
 def check_ip_running_server(ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
     result = sock.connect_ex((ip, SERVER_PORT))
     if result == 0:
-        print ("Port is open")
+        return True
     else:
-        print("Port is closed")
+        return False
 
     sock.close()
+
+
+
+def autodetect_server(own_ip):
+    ip_parts = own_ip.split('.')
+    subnet = ip_parts[:3]
+
+    for i in range(1, 254):
+        check_ip = ".".join(subnet + [str(i)])
+        print(check_ip)
+        if check_ip_running_server(check_ip):
+            return check_ip
+
+    return None
+
+# autodetect_server(get_own_ip())
