303fc3e045a6ce02d13d090baf25b65b2b578d55
Uli <admin@deruli.de>
2024-03-02 02:37:24 +0100

    Add get_Ip, some visual chanfges

--- src/utils/netcode.py
+++ src/utils/netcode.py
@@ -1,17 +1,28 @@
 import socket
 
-def get_ip():
+# TODO which port to use?
+SERVER_PORT = 19132
+
+def get_own_ip():
+    """ Get own ip for network multiplayer """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
     try:
         # doesn't even have to be reachable
-        s.connect(('0.0.0.0', 1))
+        s.connect(('123.123.123.123', 1))
         IP = s.getsockname()[0]
     except Exception:
-        IP = '127.0.0.1'
+        return None
     finally:
         s.close()
     return IP
 
+def check_ip_running_server(ip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((ip, SERVER_PORT))
+    if result == 0:
+        print ("Port is open")
+    else:
+        print("Port is closed")
 
-print(get_ip())
+    sock.close()
