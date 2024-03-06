bded202fa9314ae0fedef6a240f5ccd41a1f3d15
Your Name <you@example.com>
2024-03-06 23:44:39 +0100

    Scene

--- src/utils/scene.py
+++ src/utils/scene.py
@@ -1,7 +1,17 @@
-def get_layer(name, scene):
+""" Scene utils """
+from arcade import Scene, SpriteList
+
+
+def get_layer(name: str, scene: Scene) -> SpriteList:
+    """
+    Get layer from scene
+    @param name: Name of layer
+    @param scene: Scene
+    @return: List of sprites
+    """
     try:
         sprites = scene.get_sprite_list(name)
     except KeyError:
-        sprites = []
+        sprites = SpriteList()
 
     return sprites
