86e24ece9e6c5a25fb0cc538b608adb7c41213e0
Your Name <you@example.com>
2024-03-09 02:14:30 +0100

    updated controls page

--- src/setup.py
+++ src/setup.py
@@ -29,7 +29,7 @@ target = cx_Freeze.Executable(
 options = {
     'build_exe': {
         # "include_msvcr": True, Not allowed to legal reasons
-        'optimize': 0,
+        'optimize': 2,
         'include_files': [
             'data/',
             '../CREDITS.txt',
