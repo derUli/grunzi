8044300a297b7b0298b8f095dbb8d5592c0d1bd1
Your Name <you@example.com>
2024-03-09 12:42:55 +0100

    Add V-Sync toggle

--- src/views/optionsmenu.py
+++ src/views/optionsmenu.py
@@ -189,8 +189,8 @@ class OptionsMenu(Fading):
         self.draw_debug()
 
     def get_texture_by_value(self, width, height, value=False):
-        red_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.RED)
-        green_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.GREEN)
+        red_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.BLACK)
+        green_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.HOTPINK)
 
         texture_red = arcade.texture.Texture(name='red_background', image=red_background)
         texture_green = arcade.texture.Texture(name='green_background', image=green_background)
