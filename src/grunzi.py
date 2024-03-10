0053dbcf244cce721e8be5f9a7384ca57bd09346
Your Name <you@example.com>
2024-03-10 12:18:36 +0100

    grunzi modified

--- src/grunzi.py
+++ src/grunzi.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
+""" Starts the game """
+
 import logging
 import os
 import sys
 
 from views.startup import StartUp
 
-ROOT_DIR = os.path.dirname(__file__)
 
-if __name__ == "__main__":
+def start() -> None:
+    """ Starts the game """
     try:
-        StartUp(ROOT_DIR).main()
-    except KeyboardInterrupt as e:
+        StartUp(os.path.dirname(__file__)).main()
+    except KeyboardInterrupt:
         sys.exit()
     except Exception as e:
         logging.fatal("Fatal exception", exc_info=e)
         sys.exit()
+
+
+if __name__ == "__main__":
+    start()
