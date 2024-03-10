9f3e68490eba1cae3011ff13a08b693b2e446082
Your Name <you@example.com>
2024-03-10 22:31:56 +0100

    volume

--- src/utils/gui.py
+++ src/utils/gui.py
@@ -1,12 +1,21 @@
+""" GUI utils """
 import PIL
 import arcade
+from arcade import Texture
 from arcade.gui import UIFlatButton
 
 from constants.fonts import FONT_DEFAULT
 from utils.text import MEDIUM_FONT_SIZE
 
 
-def get_texture_by_value(width, height, value=False):
+def get_texture_by_value(width: int, height: int, value: bool = False) -> Texture:
+    """
+    Get a solid color background texture for toggle button based on it's value
+    @param width: The width
+    @param height: The height
+    @param value: The bool value
+    @return: Texture
+    """
     red_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.BLACK)
     green_background = PIL.Image.new("RGBA", (width, height), arcade.csscolor.HOTPINK)
 
