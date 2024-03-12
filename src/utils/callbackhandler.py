b6aeffb54d30f79b15f3806f6b20da826c944ad0
Your Name <you@example.com>
2024-03-12 21:15:02 +0100

    size Property weg

--- src/utils/callbackhandler.py
+++ src/utils/callbackhandler.py
@@ -1,3 +1,8 @@
+""" Callback handlers """
+
+
 class CallbackHandler:
+    """ Callback handlers passed to a sprite on use """
+
     def __init__(self, on_complete):
         self.on_complete = on_complete
