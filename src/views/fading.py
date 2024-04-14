1bea8e6a67ed1485d9c488a98bdad746943a75de
Your Name <you@example.com>
2024-04-14 13:28:21 +0200

    comment

--- src/views/fading.py
+++ src/views/fading.py
@@ -18,7 +18,10 @@ class Fading(View):
         self._fade_in = 255
         self._do_quit = False
 
-    def update_fade(self, next_view=None) -> None:
+    def update_fade(self, next_view: arcade.View | None = None) -> None:
+        """ Update fade
+            @param next_view: View to show after finishing fade
+        """
         if self._fade_out is not None:
             self._fade_out += FADE_RATE
             if self._fade_out is not None and self._fade_out > 255 and next_view is not None:
@@ -32,7 +35,8 @@ class Fading(View):
             if self._fade_in <= 0:
                 self._fade_in = None
 
-    def fade_out(self):
+    def fade_out(self) -> None:
+        """ Start fadeout """
         if self._fade_out is not None:
             return
         self._fade_out = 0
