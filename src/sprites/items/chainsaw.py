ae3e6c53a491806d2ccf85272f5df10d959f462f
Your Name <you@example.com>
2024-05-01 12:04:42 +0200

    sound

--- src/sprites/items/chainsaw.py
+++ src/sprites/items/chainsaw.py
@@ -9,9 +9,9 @@ class Chainsaw(Item):
         from sprites.characters.skullsprite import SkullSprite
 
         if isinstance(b, Tree):
-            if b.fade_destroy():
-                sound_number = random.randint(1, 4)
-                state.play_sound('chainsaw' + str(sound_number))
+            b.fade_destroy()
+            sound_number = random.randint(1, 4)
+            state.play_sound('chainsaw' + str(sound_number))
             return
 
         if isinstance(b, SkullSprite):
