6afeaba456d2216d5c4bb4d7b67b4ef35169794f
Your Name <you@example.com>
2024-05-09 19:06:06 +0200

    code formatiert

--- src/sprites/items/chainsaw.py
+++ src/sprites/items/chainsaw.py
@@ -2,6 +2,7 @@ import random
 
 from arcade import FACE_DOWN, FACE_UP
 
+from sprites.characters.chicken import Chicken
 from sprites.items.item import Item, Tree
 
 
@@ -11,15 +12,24 @@ class Chainsaw(Item):
 
         if isinstance(b, Tree):
             b.fade_destroy()
-            sound_number = random.randint(1, 4)
-            state.play_sound('chainsaw' + str(sound_number))
+            self.play_sound(state)
             return
 
         if isinstance(b, SkullSprite):
             b.hurt(50)
+            self.play_sound(state)
+            return
+
+        if isinstance(b, Chicken):
+            b.hurt(100)
+            self.play_sound(state)
             return
 
         state.beep()
+
+    def play_sound(self, state):
+        sound_number = random.randint(1, 4)
+        state.play_sound('chainsaw' + str(sound_number))
 
     def generate_rotated(self, image):
         rotated = super().generate_rotated(image)
