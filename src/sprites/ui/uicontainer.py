a0237249970049665e30d5fd4e6fc077d8153b11
Your Name <you@example.com>
2024-05-10 13:13:25 +0200

    UI

--- src/sprites/ui/uicontainer.py
+++ src/sprites/ui/uicontainer.py
@@ -3,11 +3,14 @@ from sprites.ui.inventorycontainer import InventoryContainer
 
 class UIContainer:
     def __init__(self):
+        """ Constructor """
         self.inventory = None
 
     def setup(self, state, size):
+        """ Setup UI """
         self.inventory = InventoryContainer()
         self.inventory.setup(state=state, size=size)
 
     def draw(self):
+        """ Draw UI """
         self.inventory.draw()
