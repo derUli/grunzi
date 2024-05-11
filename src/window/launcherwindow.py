d446b99581cf3311e9141367ced1eb48251c1bec
Your Name <you@example.com>
2024-05-11 19:23:14 +0200

    README

--- src/window/launcherwindow.py
+++ src/window/launcherwindow.py
@@ -15,6 +15,31 @@ SPACE_BETWEEN = 2
 
 TTK_THEME = 'equilux'
 
+SETTINGS_LOW = 'low'
+SETTINGS_MEDIUM = 'medium'
+SETTINGS_HIGH = 'high'
+
+SETTINGS_PRESETS = {
+    SETTINGS_LOW: {
+        'shaders': False,
+        'traffic': False,
+        'sky': False,
+        'videos': False
+    },
+    SETTINGS_MEDIUM: {
+        'shaders': False,
+        'traffic': True,
+        'sky': True,
+        'videos': True
+    },
+    SETTINGS_HIGH: {
+        'shaders': True,
+        'traffic': True,
+        'sky': True,
+        'videos': True
+    }
+}
+
 
 class LauncherWindow(ThemedTk):
     def __init__(self, theme=TTK_THEME, args=None, state=None):
