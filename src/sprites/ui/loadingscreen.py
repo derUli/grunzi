e0ccb180f56bfc0b76a7e2710eaac0fef80b487d
Your Name <you@example.com>
2024-05-12 13:40:57 +0200

    Progressbar

--- src/sprites/ui/loadingscreen.py
+++ src/sprites/ui/loadingscreen.py
@@ -1,14 +1,21 @@
+import arcade
+
 from state.viewstate import ViewState
 from utils.text import create_text, MARGIN, EXTRA_LARGE_FONT_SIZE
+
+BAR_HEIGHT = 30
 
 
 class LoadingScreen:
     def __init__(self):
-
+        self.size = None
         self.shadertoy = None
 
         self.loading_text = None
         self.time = 0
+
+        self.onepercent = None
+        self._percent = 100
         # TODO: Progress bar
 
     def setup(self, state: ViewState, size: tuple):
@@ -16,6 +23,9 @@ class LoadingScreen:
             self.shadertoy = state.load_shader(size, 'gameover')
 
         w, h = size
+        self.size = size
+
+        self.onepercent = w / 100
 
         self.loading_text = create_text(
             _("Loading..."),
@@ -30,5 +40,16 @@ class LoadingScreen:
     def draw(self, time=None):
         if self.shadertoy:
             self.shadertoy.render(time=time)
+
+        bar_width = self._percent * self.onepercent
+        w, h = self.size
+
+        arcade.draw_rectangle_filled(
+            w / 2,
+            BAR_HEIGHT / 2,
+            bar_width,
+            BAR_HEIGHT,
+            arcade.csscolor.HOTPINK
+        )
 
         self.loading_text.draw()
