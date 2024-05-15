bbd6ca550fc13a4cef784c5dc4ceec209bb97bec
Your Name <you@example.com>
2024-05-16 00:36:49 +0200

    comments

--- src/utils/keypressed.py
+++ src/utils/keypressed.py
@@ -1,11 +1,19 @@
+""" Used to store pressed keys"""
+
+
 class KeyPressed:
+    """ Class to store pressed keys"""
+
     def __init__(self):
+        """ Constructor """
+
         self.key_up = False
         self.key_right = False
         self.key_down = False
         self.key_left = False
 
     def reset(self):
+        """ Reset values """
         self.key_up = False
         self.key_right = False
         self.key_down = False
