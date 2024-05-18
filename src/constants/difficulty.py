e2f8145ecd87ad3753bb75732ba34ecb1c091a95
Your Name <you@example.com>
2024-05-19 00:52:59 +0200

    comments

--- src/constants/difficulty.py
+++ src/constants/difficulty.py
@@ -20,12 +20,19 @@ class Difficulty:
 
         self.setup(difficulty, map_name, level_dir)
 
+    def reset(self):
+        """ Reset values """
+
+        self.max_skulls = 0
+        self.skull_spawn_range = 0
+        self.skull_hurt = 0
+
     def setup(self, difficulty, map_name, level_dir) -> None:
         """ Setup difficulty """
 
         maps = os.path.join(level_dir, 'maps.json')
 
-        with open(maps, 'r') as f:
+        with open(maps, 'r', encoding='utf-8') as f:
             jsond = json.load(f)
 
         difficulty_data = jsond['default'][str(difficulty)]
@@ -33,9 +40,7 @@ class Difficulty:
         if map_name in jsond:
             difficulty_data = jsond[map_name][str(difficulty)]
 
-        self.max_skulls = 0
-        self.skull_spawn_range = 0
-        self.skull_hurt = 0
+        self.reset()
 
         if 'maxSkulls' in difficulty_data:
             self.max_skulls = difficulty_data['maxSkulls']
