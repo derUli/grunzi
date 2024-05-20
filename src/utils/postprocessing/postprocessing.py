96d3f531fb39889fe446051dab33c29f98e5e96b
Your Name <you@example.com>
2024-05-20 21:03:00 +0200

    Sun effect

--- src/utils/postprocessing/postprocessing.py
+++ src/utils/postprocessing/postprocessing.py
@@ -15,10 +15,10 @@ class PostProcessing:
                 Fog().setup(args)
             )
 
-        if args.state.difficulty.options['sun']:
-            self.pipeline.append(
-                Sun().setup(args)
-            )
+        # if args.state.difficulty.options['sun']:
+        #     self.pipeline.append(
+        #         Sun().setup(args)
+        #     )
         return self
 
     def update(self, delta_time, args):
