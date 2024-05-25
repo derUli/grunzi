579d2621a0332f9d052bbd48e48b5913152d4a1d
Your Name <you@example.com>
2024-05-25 15:29:10 +0200

    add lantern

--- src/sprites/items/lantern.py
+++ src/sprites/items/lantern.py
@@ -4,17 +4,17 @@ from arcade import FACE_DOWN, FACE_RIGHT, FACE_LEFT
 
 from sprites.items.fence import Fence
 from sprites.items.item import Item
-
+from utils.lightmanager import LIGHT_LAYER_LANTERN
 SCORE_DESTROY_FENCE = 100
 
 
 class Lantern(Item):
+    
     def on_equip(self, args):
-        logging.info('Equip lantern')
-
+        args.scene.light_manager.enable_layer(LIGHT_LAYER_LANTERN)
         
     def on_unequip(self, args):
-        logging.info('Unequip lantern')
+        args.scene.light_manager.disable_layer(LIGHT_LAYER_LANTERN)
 
     def copy(self):
         """ Copy item """
@@ -23,3 +23,7 @@ class Lantern(Item):
             center_x=self.center_x,
             center_y=self.center_y
         )
+
+    def update(self, delta_time, args):
+        args.scene.light_manager.enable_layer(LIGHT_LAYER_LANTERN)
+
--- src/utils/lightmanager.py
+++ src/utils/lightmanager.py
@@ -2,7 +2,7 @@ import arcade
 from arcade.experimental.lights import LightLayer, Light
 
 LIGHTING_DARK = 'dark'
-
+LIGHT_LAYER_LANTERN = 'lantern'
 
 class LightManager:
     def __init__(self):
@@ -30,20 +30,8 @@ class LightManager:
         if self._type == LIGHTING_DARK:
             self.setup_dark(args)
 
-    def update(self, args):
-        if not self.enabled:
-            return
-
-        self.lights['player_light'].position = args.player.position
-
-    def draw(self):
-        if not self.enabled:
-            return
-
-        self.light_layer.draw()
-
     def setup_dark(self, args):
-        self.lights['player_light'] = Light(
+        self.lights[LIGHT_LAYER_LANTERN] = Light(
             args.player.center_x,
             args.player.center_y,
             150,
@@ -51,8 +39,52 @@ class LightManager:
             'soft'
         )
 
-        self.light_layer.add(self.lights['player_light'])
+        
+    def update(self, args):
+        if not self.enabled:
+            return
+
+        if self._type == LIGHTING_DARK:
+            self.update_dark(args)
+
+    def update_dark(self, args):
+        
+        from constants.layers import LAYER_LANTERN
+
+        source = arcade.sprite.SpriteSolidColor(1, 1, arcade.csscolor.BLACK)
+
+        if len(args.scene[LAYER_LANTERN]) > 0:
+            source = args.scene[LAYER_LANTERN][0]
+
+        from sprites.items.lantern import Lantern
+
+        if isinstance(args.player.get_item(), Lantern):
+            source = args.player.get_item()
+
+        self.lights[LIGHT_LAYER_LANTERN].position = source.position
+
+
+    def draw(self):
+        if not self.enabled:
+            return
+
+        self.light_layer.draw()
+
 
     @property
     def enabled(self):
         return self._type is not None
+
+    def enable_layer(self, name):
+        if self.lights[name] not in self.light_layer:
+            self.light_layer.add(self.lights[name])
+
+    def disable_layer(self, name):
+        if self.lights[name] in self.light_layer:
+            self.light_layer.remove(self.lights[name])
+
+    def toggle_layer(self, name):
+        if self.lights[name] in self.light_layer:
+            self.disable_layer()
+        else:
+            self.enable_layer()
