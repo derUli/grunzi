598c4031d78bc2bdaf5c75d579f8e4ebdf454678
Your Name <you@example.com>
2024-06-05 09:27:55 +0200

    code

--- src/views/difficultyselection.py
+++ src/views/difficultyselection.py
@@ -187,8 +187,7 @@ class DifficultySelection(Fading):
     def on_back(self) -> None:
         """ On click "Back" button """
         from views.mainmenu import MainMenu
-        self.next_view = MainMenu(self.window, self.state)
-        self.fade_out()
+        self.fade_to_view(MainMenu(self.window, self.state))
 
     def on_confirm_overwrite_savegame(self) -> None:
         """ Show confirm overwrite savegame dialog """
