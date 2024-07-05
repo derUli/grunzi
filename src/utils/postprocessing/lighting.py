55d0d90f44fc56f6116136fb64431549f8c0ba45
Your Name <you@example.com>
2024-07-05 08:56:38 +0200

    Highscore

---
+++ src/utils/postprocessing/lighting.py
@@ -0,0 +1,86 @@
+from typing import Self
+
+import arcade
+
+from state.argscontainer import ArgsContainer
+from utils.postprocessing.effect import Effect
+
+DEFAULT_ALPHA = 150
+
+MOVE_SPEED = 0.33
+
+
+class Lighting(Effect):
+    def setup(self, args: ArgsContainer) -> Self:
+        """
+        Setup fog effect
+        @param args: ArgsContainer
+        @return: Self
+        """
+
+        self.spritelist.clear()
+
+        w, h = arcade.get_window().get_size()
+
+        sprite = arcade.sprite.SpriteSolidColor(w, h, color=(124, 252, 0, 255))
+        sprite.alpha = 0
+        self.spritelist.append(sprite)
+
+        return self
+
+    def update(self, delta_time: float, args: ArgsContainer) -> None:
+        self.update_green(args)
+
+    def update_green(self, args: ArgsContainer):
+        w, h = arcade.get_window().get_size()
+        x, y = args.player.position
+
+        if x < w * 0.5:
+            x = w * 0.5
+
+        if y < h * 0.5:
+            y = h * 0.5
+
+        self.spritelist[0].position = (x, y)
+
+        import sprites.bullet.slimerbullet
+        from constants.layers import LAYER_NPC
+
+        if LAYER_NPC not in args.scene.name_mapping:
+            return
+
+        alpha = 0
+        radius = h
+        max_alpha = 100
+        one = max_alpha / radius
+
+        found = False
+
+        for sprite in args.scene[LAYER_NPC]:
+            if isinstance(sprite, sprites.bullet.slimerbullet.SlimerBullet):
+                distance = arcade.get_distance_between_sprites(sprite, args.player)
+
+                if distance > radius:
+                    continue
+
+                found = True
+
+                new_alpha = max_alpha - (one * distance)
+                if new_alpha > alpha:
+                    alpha = new_alpha
+
+        if alpha > max_alpha:
+            alpha = max_alpha
+
+        if not found:
+            alpha = self.spritelist[0].alpha - 1
+
+            if alpha < 0:
+                alpha = 0
+
+        self.spritelist[0].alpha = alpha
+
+    def draw(self) -> None:
+        """ Draw fog """
+
+        self.spritelist.draw()
--- src/utils/postprocessing/postprocessing.py
+++ src/utils/postprocessing/postprocessing.py
@@ -1,5 +1,6 @@
 import arcade.sprite
 from utils.postprocessing.fog import Fog
+from utils.postprocessing.lighting import Lighting
 
 
 class PostProcessing:
@@ -14,65 +15,15 @@ class PostProcessing:
                 Fog().setup(args)
             )
 
+        self.pipeline.append(
+            Lighting().setup(args)
+        )
+
         return self
 
     def update(self, delta_time, args) -> None:
         for effect in self.pipeline:
             effect.update(delta_time, args)
-
-        w, h = arcade.get_window().get_size()
-
-        if not self.sprite:
-            self.sprite = arcade.sprite.SpriteSolidColor(w, h, color=(124, 252, 0, 255))
-            self.sprite.alpha = 0
-
-        x, y = args.player.position
-
-        if x < w * 0.5:
-            x = w * 0.5
-
-        if y < h * 0.5:
-            y = h * 0.5
-
-        self.sprite.position = (x, y)
-
-        import sprites.bullet.slimerbullet
-        from constants.layers import LAYER_NPC
-
-        if LAYER_NPC not in args.scene.name_mapping:
-            return
-
-        alpha = 0
-        radius = h
-        max_alpha = 100
-        one = max_alpha / radius
-
-        found = False
-
-        for sprite in args.scene[LAYER_NPC]:
-            if isinstance(sprite, sprites.bullet.slimerbullet.SlimerBullet):
-                distance = arcade.get_distance_between_sprites(sprite, args.player)
-
-                if distance > radius:
-                    continue
-
-                found = True
-
-                new_alpha = max_alpha - (one * distance)
-                if new_alpha > alpha:
-                    alpha = new_alpha
-
-        if alpha > max_alpha:
-            alpha = max_alpha
-
-        if not found:
-            alpha = self.sprite.alpha - 1
-
-            if alpha < 0:
-                alpha = 0
-
-        self.sprite.alpha = alpha
-
 
     def draw(self) -> None:
         """ Draw all postprocessing effects """
