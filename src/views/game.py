dade7856843003677ca447e72b35cf1904fdbe9a
Your Name <you@example.com>
2024-07-05 12:43:38 +0200

    Highscore

--- src/views/game.py
+++ src/views/game.py
@@ -339,9 +339,9 @@ class Game(Fading):
         self.draw_after()
 
     def update_player_speed(self) -> None:
-        self.window.set_mouse_visible(False)
-
         """ Update player sprite """
+
+        self.window.set_mouse_visible(False)
 
         # Calculate speed based on the keys pressed
         self.player_sprite.change_x, self.player_sprite.change_y = 0, 0
