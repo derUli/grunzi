5e035a57a7ac290c2acdc8e8e4878b3bcceeca21
Your Name <you@example.com>
2024-07-05 16:33:49 +0200

    Highscore

--- src/utils/postprocessing/postprocessing.py
+++ src/utils/postprocessing/postprocessing.py
@@ -1,5 +1,10 @@
+import os
+
+import arcade
+
 from utils.postprocessing.colortint import ColorTint
 from utils.postprocessing.fog import Fog
+from utils.sprite import load_animated_gif
 
 
 class PostProcessing:
@@ -18,14 +23,32 @@ class PostProcessing:
             ColorTint().setup(args)
         )
 
+        w, h = arcade.get_window().get_size()
+        self.filmgrain = load_animated_gif(
+            os.path.join(args.state.animation_dir, 'grain.gif'),
+            (w, h)
+        )
+        self.filmgrain.alpha = 40
+        self.filmgrain.position = args.player.position
+
         return self
 
     def update(self, delta_time, args) -> None:
         for effect in self.pipeline:
             effect.update(delta_time, args)
 
+        w, h = arcade.get_window().get_size()
+
+        self.filmgrain.position = args.player.position
+        self.filmgrain.center_x = max(w * 0.5, self.filmgrain.center_x)
+        self.filmgrain.center_y = max(h * 0.5, self.filmgrain.center_y)
+        self.filmgrain.update_animation(delta_time)
+
+
     def draw(self) -> None:
         """ Draw all postprocessing effects """
 
         for effect in self.pipeline:
             effect.draw()
+
+        self.filmgrain.draw()
--- src/views/game.py
+++ src/views/game.py
@@ -76,6 +76,8 @@ class Game(Fading):
         self.wall_spritelist = None
         self.map_populator = None
 
+        self.filmgrain = None
+
     def on_show_view(self) -> None:
         """ On show view """
         super().on_show_view()
