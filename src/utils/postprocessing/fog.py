3917f9d04c904cca9c1d28030ecf110b8854cd45
Your Name <you@example.com>
2024-07-05 03:10:52 +0200

    Highscore

--- src/utils/postprocessing/fog.py
+++ src/utils/postprocessing/fog.py
@@ -9,7 +9,7 @@ from PIL.Image import Resampling
 from state.argscontainer import ArgsContainer
 from utils.postprocessing.effect import Effect
 
-DEFAULT_ALPHA = 140
+DEFAULT_ALPHA = 150
 
 MOVE_SPEED = 0.33
 
