6d0576917d34b7accd308368f50750ff6e8072ee
Your Name <you@example.com>
2024-07-07 10:34:14 +0200

    code qualität

--- tests/test_utils_screen.py
+++ tests/test_utils_screen.py
@@ -6,4 +6,4 @@ from utils.screen import supported_screen_resolutions
 class ScreenTest(unittest.TestCase):
 
     def test_screen_resolutions(self):
-        self.assertIn('800x600', supported_screen_resolutions())
+        self.assertIn('1024x768', supported_screen_resolutions())
