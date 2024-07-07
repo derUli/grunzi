33d277853eab47cf573c995af803c912188a10ab
Your Name <you@example.com>
2024-07-07 23:33:05 +0200

    Tests

--- highscore/index.php
+++ highscore/index.php
@@ -1,26 +1,24 @@
-<?php 
-$name = $_GET['name'] ?? null;
-$score = $_GET['score'] ? (int)$_GET['score'] : null;
+<?php
+$name = $_GET["name"] ?? null;
+$score = $_GET["score"] ? (int) $_GET["score"] : null;
 
-$data = json_decode(file_get_contents('data.json'), true);
+$data = json_decode(file_get_contents("data.json"), true);
 
-if(!empty($name) && !empty($score)){
+if (!empty($name) && !empty($score)) {
     $data[] = [
-        'name' => substr($name, 0, 20),
-        'score' => $score
+        "name" => substr($name, 0, 20),
+        "score" => $score,
     ];
 
-    file_put_contents('data.json', json_encode($data, true));
+    file_put_contents("data.json", json_encode($data, true));
 }
 
+header("Content-Type: application/json; charset=UTF-8");
 
-header('Content-Type: application/json; charset=UTF-8');
-
-usort($data, function($a, $b){
-    return $a['score'] < $b['score'] ? 1 : -1;
+usort($data, function ($a, $b) {
+    return $a["score"] < $b["score"] ? 1 : -1;
 });
-
 
 $data = array_slice($data, 0, 10);
 echo json_encode($data);
-exit;
+exit();
