40e30d0b5585b7dc33f80a5578169f64ce607946
Your Name <you@example.com>
2024-07-08 01:39:35 +0200

    Tests

--- tests/test_utils_text.py
+++ tests/test_utils_text.py
@@ -4,7 +4,7 @@ import unittest
 import arcade
 
 from state.viewstate import ViewState
-from utils.gui import get_button_style
+from utils.gui import get_button_style, get_slider_style
 from utils.text import label_value, create_text
 from window.gamewindow import GameWindow
 
@@ -29,7 +29,13 @@ class TextTest(unittest.TestCase):
     def test_get_button_style(self):
         self.assertEqual('Laila', get_button_style()['hover']['font_name'])
 
-    def test_create_text(self):
-        text = create_text('Foobar')
+    def test_get_slider_style(self):
+        slider_style = get_slider_style()
+        self.assertIsInstance(slider_style, dict)
 
+        for val in slider_style.values():
+            self.assertIsInstance(val, arcade.gui.UISlider.UIStyle)
+    def test_create_text(self):
+        
+        text = create_text('Foobar')
         self.assertIsInstance(text, arcade.text.Text)
