ccb36aac6fe3095c5b48f460ab7e906651b4bed8
Your Name <you@example.com>
2024-07-09 22:03:58 +0200

    Tests

--- tests/test_utils_physics.py
+++ tests/test_utils_physics.py
@@ -1,10 +1,11 @@
 import os
 import unittest
 
+import arcade.sprite
 from arcade import Scene, PymunkPhysicsEngine, SpriteList
 
 from sprites.characters.player import Player
-from utils.physics import make_physics_engine
+from utils.physics import make_physics_engine, on_hit_destroy
 
 
 class PhysicsTest(unittest.TestCase):
@@ -16,3 +17,14 @@ class PhysicsTest(unittest.TestCase):
         scene.add_sprite_list('Walls', SpriteList())
         scene.add_sprite_list('Moveable', SpriteList())
         self.assertIsInstance(make_physics_engine(sprite, scene), PymunkPhysicsEngine)
+
+    def test_on_hit_destroy(self):
+        sprites = SpriteList()
+
+        sprites.append(
+            arcade.sprite.SpriteSolidColor(color=arcade.color.RED, width=60, height=60)
+        )
+
+        on_hit_destroy(sprites[0], None, None, None, None)
+
+        self.assertEqual(len(sprites), 0)
