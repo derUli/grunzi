38060c57aa65b30193977c7a65dbaaace44c5164
Your Name <you@example.com>
2024-08-17 22:44:40 +0200

    Center Mouse Pointer

--- src/sprites/characters/fish.py
+++ src/sprites/characters/fish.py
@@ -49,7 +49,7 @@ class Fish(Character):
 
         if not any(args.scene[LAYER_RIVER]):
             if self.angle > -180:
-                self.turn_right(10)
+                self.turn_right(20)
             return
 
         self.center_x -= MOVE_SPEED_X
