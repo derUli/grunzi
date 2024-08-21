9d0b6ff9a670d627a42aa19c2e6b0612be0ec764
Your Name <you@example.com>
2024-08-21 13:04:44 +0200

    loader

---
+++ src/utils/loader/Loader.py
@@ -0,0 +1,119 @@
+import logging
+import os
+import sys
+import time
+
+import arcade
+import pyglet
+
+from utils.media.audio import MusicQueue
+from constants.layers import LAYER_OPTIONS
+from constants.mapconfig import MapConfig
+from sprites.characters.player import Player
+from state.argscontainer import make_args_container
+from state.savegamestate import SaveGameState
+from utils.callbackhandler import CallbackHandler
+from utils.mappopulator import MapPopulator
+from utils.physics import make_physics_engine
+from utils.scene import Scene
+from utils.tilemap import TileMap
+from window.gamewindow import UPDATE_RATE
+
+
+class Loader:
+
+    """ Async load map """
+    def run(self, klaas) -> None:
+
+        klaas.ui.loading_screen.show = True
+        klaas.ui.loading_screen.percent = 0
+
+        savegame = SaveGameState.load()
+        savegame.current = klaas.state.map_name
+        savegame.save()
+
+        # Set up the Cameras
+        klaas.camera_sprites = arcade.Camera()
+        klaas.state.reset()
+        klaas.state.difficulty = MapConfig(savegame.difficulty, klaas.state.map_name, klaas.state.map_dir)
+
+        klaas.ui.loading_screen.percent = 10
+
+        # Name of map file to load
+        map_name = os.path.join(klaas.state.map_dir, f"{klaas.state.map_name}.tmx")
+
+        # Read in the tiled map
+        try:
+            klaas.tilemap = TileMap(
+                map_name,
+                layer_options=LAYER_OPTIONS
+            )
+        except FileNotFoundError as e:
+            logging.error(e)
+            sys.exit(1)
+
+        klaas.ui.loading_screen.percent = 25
+
+        # Initialize Scene with our TileMap, this will automatically add all layers
+        # from the map as SpriteLists in the scene in the proper order.
+        klaas.scene = Scene.from_tilemap(klaas.tilemap.map)
+
+        klaas.ui.loading_screen.percent = 50
+
+        klaas.map_populator = MapPopulator()
+        klaas.map_populator.spawn_initial(make_args_container(klaas))
+
+        # Set up the player, specifically placing it at these coordinates.
+        filename = os.path.join(klaas.state.sprite_dir, 'char', 'pig.png')
+        klaas.player_sprite = Player(filename)
+
+        klaas.player_sprite.setup(
+            state=klaas.state,
+            scene=klaas.scene,
+            callbacks=CallbackHandler(
+                on_complete=klaas.on_next_level
+            ),
+            controllers=klaas.window.controllers
+        )
+
+        # Create the physics engine
+        klaas.physics_engine = make_physics_engine(klaas.player_sprite, klaas.scene)
+
+        klaas.map_populator.spawn_npcs(make_args_container(klaas))
+
+        klaas.ui.loading_screen.percent = 60
+
+        klaas.wall_spritelist = klaas.scene.make_wall_spritelist()
+
+        sprite = arcade.SpriteSolidColor(
+            width=64,
+            height=64,
+            color=arcade.color.BLACK
+        )
+
+        klaas.astar_barrier_list = arcade.AStarBarrierList(
+            moving_sprite=sprite,
+            blocking_sprites=klaas.wall_spritelist,
+            grid_size=64,
+            left=0,
+            right=klaas.tilemap.width,
+            top=klaas.tilemap.height,  # FIXME: Top and bottom is switched in this dev version of arcade
+            bottom=0
+        )
+
+        klaas.ui.loading_screen.percent = 80
+
+        # Create the music queue
+        klaas.music_queue = MusicQueue(state=klaas.state)
+        klaas.music_queue.from_directory(os.path.join(klaas.state.music_dir, str(klaas.state.map_name)))
+
+        klaas.ui.loading_screen.percent = 100
+
+        pyglet.clock.schedule_interval_soft(klaas.wait_for_video, interval=UPDATE_RATE)
+
+        # Sleep some seconds to wait until the 100 Percent is shown
+        while not klaas.ui.loading_screen.completed:
+            time.sleep(0.0001)
+
+        klaas.ui.loading_screen.show = False
+        klaas.initialized = True
--- src/views/game.py
+++ src/views/game.py
@@ -20,6 +20,7 @@ from sprites.ui.uicontainer import UIContainer
 from state.argscontainer import make_args_container
 from state.savegamestate import SaveGameState
 from utils.callbackhandler import CallbackHandler
+from utils.loader.Loader import Loader
 from utils.mappopulator import MapPopulator
 from utils.media.video import load_video, Video
 from utils.physics import make_physics_engine
@@ -131,102 +132,11 @@ class Game(Fading):
         threading.Thread(target=self.async_load).start()
 
     def async_load(self) -> None:
-        """ Async load map """
 
         start_time = time.time()
 
-        self.ui.loading_screen.show = True
-        self.ui.loading_screen.percent = 0
-
-        savegame = SaveGameState.load()
-        savegame.current = self.state.map_name
-        savegame.save()
-
-        # Set up the Cameras
-        self.camera_sprites = arcade.Camera()
-        self.state.reset()
-        self.state.difficulty = MapConfig(savegame.difficulty, self.state.map_name, self.state.map_dir)
-
-        self.ui.loading_screen.percent = 10
-
-        # Name of map file to load
-        map_name = os.path.join(self.state.map_dir, f"{self.state.map_name}.tmx")
-
-        # Read in the tiled map
-        try:
-            self.tilemap = TileMap(
-                map_name,
-                layer_options=LAYER_OPTIONS
-            )
-        except FileNotFoundError as e:
-            logging.error(e)
-            sys.exit(1)
-
-        self.ui.loading_screen.percent = 25
-
-        # Initialize Scene with our TileMap, this will automatically add all layers
-        # from the map as SpriteLists in the scene in the proper order.
-        self.scene = Scene.from_tilemap(self.tilemap.map)
-
-        self.ui.loading_screen.percent = 50
-
-        self.map_populator = MapPopulator()
-        self.map_populator.spawn_initial(make_args_container(self))
-
-        # Set up the player, specifically placing it at these coordinates.
-        filename = os.path.join(self.state.sprite_dir, 'char', 'pig.png')
-        self.player_sprite = Player(filename)
-
-        self.player_sprite.setup(
-            state=self.state,
-            scene=self.scene,
-            callbacks=CallbackHandler(
-                on_complete=self.on_next_level
-            ),
-            controllers=self.window.controllers
-        )
-
-        # Create the physics engine
-        self.physics_engine = make_physics_engine(self.player_sprite, self.scene)
-
-        self.map_populator.spawn_npcs(make_args_container(self))
-
-        self.ui.loading_screen.percent = 60
-
-        self.wall_spritelist = self.scene.make_wall_spritelist()
-
-        sprite = arcade.SpriteSolidColor(
-            width=64,
-            height=64,
-            color=arcade.color.BLACK
-        )
-
-        self.astar_barrier_list = arcade.AStarBarrierList(
-            moving_sprite=sprite,
-            blocking_sprites=self.wall_spritelist,
-            grid_size=64,
-            left=0,
-            right=self.tilemap.width,
-            top=self.tilemap.height,  # FIXME: Top and bottom is switched in this dev version of arcade
-            bottom=0
-        )
-
-        self.ui.loading_screen.percent = 80
-
-        # Create the music queue
-        self.music_queue = utils.media.audio.MusicQueue(state=self.state)
-        self.music_queue.from_directory(os.path.join(self.state.music_dir, str(self.state.map_name)))
-
-        self.ui.loading_screen.percent = 100
-
-        pyglet.clock.schedule_interval_soft(self.wait_for_video, interval=UPDATE_RATE)
-
-        # Sleep some seconds to wait until the 100 Percent is shown
-        while not self.ui.loading_screen.completed:
-            time.sleep(0.0001)
-
-        self.ui.loading_screen.show = False
-        self.initialized = True
+        loader = Loader()
+        loader.run(self)
 
         logging.info(f"Map {self.state.map_name} loaded in {time.time() - start_time} seconds")
 
