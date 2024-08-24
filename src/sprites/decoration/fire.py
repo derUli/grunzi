d8b7ece8d581054cd1c039d2e8e7bd197d3c27d4
Your Name <you@example.com>
2024-08-24 22:44:58 +0200

    map

--- src/sprites/decoration/fire.py
+++ src/sprites/decoration/fire.py
@@ -1,5 +1,7 @@
 import arcade
+import pyglet.clock
 
+from sprites.characters.character import Character
 from sprites.sprite import AbstractAnimatedSprite
 from state.argscontainer import ArgsContainer
 from utils.positionalsound import PositionalSound, VOLUME_SOURCE_SOUND
@@ -23,10 +25,25 @@ class Fire(AbstractAnimatedSprite):
                 volume_source=VOLUME_SOURCE_SOUND
             )
             self.fx.update(init=True)
+            pyglet.clock.schedule_interval_soft(self.check_for_collision, 1 / 4, args)
             return
 
         self.fx.update()
 
+    def check_for_collision(self, delta_time, args):
         # Hurt player
         if arcade.check_for_collision(self, args.player):
-            args.player.hurt(2)
+            args.player.hurt(10)
+            return
+
+        from constants.layers import LAYER_NPC
+
+        if not LAYER_NPC in args.scene.name_mapping:
+            return
+
+        collisions = arcade.check_for_collision_with_list(self, args.scene[LAYER_NPC])
+
+        for collision in collisions:
+            if isinstance(collision, Character):
+                collision.hurt(10)
+                return
