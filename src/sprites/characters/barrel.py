6d7b1cab193a817bc24e723842a6c0b9ecb497d1
Your Name <you@example.com>
2024-08-24 20:21:29 +0200

    map

--- src/sprites/characters/barrel.py
+++ src/sprites/characters/barrel.py
@@ -2,31 +2,26 @@
 import os
 
 import arcade
-from arcade import FACE_RIGHT, PymunkPhysicsEngine, SpriteList
+from arcade import FACE_RIGHT
 
 from constants.collisions import COLLISION_ENEMY
-from constants.layers import LAYER_NPC, check_collision_with_layers
+from constants.layers import LAYER_NPC, check_collision_with_layers, LAYER_WALL, WALL_LAYERS
 from sprites.characters.character import Character
-from sprites.items.item import Useable
 from utils.physics import DEFAULT_FRICTION
 from utils.sprite import random_position
 
 DEFAULT_FACE = FACE_RIGHT
 
 # Physics stuff
-MOVE_FORCE = 300
+MOVE_FORCE = 300000
 MOVE_DAMPING = 0.01
+MASS = 0.05
 
 SIGHT_DISTANCE = 1400
 GRID_SIZE = 64
 
 FADE_IN_MAX = 255
 FADE_SPEED = 4
-
-SHOOT_DELTA = 1
-
-PATH_FINDING_INTERVAL = 1
-
 
 class Barrel(Character):
     def __init__(
@@ -38,6 +33,7 @@ class Barrel(Character):
         super().__init__(filename, center_x, center_y)
 
         self.damping = MOVE_DAMPING
+        self.force = MOVE_FORCE
         self.friction = DEFAULT_FRICTION
         self.fade_in = True
 
@@ -75,6 +71,34 @@ class Barrel(Character):
 
             return
 
+        if not arcade.has_line_of_sight(
+                self.position,
+                args.player.position,
+                args.wall_spritelist,
+                SIGHT_DISTANCE,
+                GRID_SIZE
+        ):
+            return
+
+
+        args.physics_engine.apply_force(self, (0, -self.force))
+
+        explodes = False
+
+        if arcade.check_for_collision(self, args.player):
+            explodes = True
+
+        for layer in WALL_LAYERS:
+            if not layer in args.scene.name_mapping:
+                break
+
+            if arcade.check_for_collision_with_list(self, args.scene[layer]):
+                explodes = True
+
+        if explodes:
+            # TODO: Explosion
+            self.health = 0
+
 
 def spawn_barrel(state, tilemap, scene, physics_engine):
     rand_x, rand_y = random_position(tilemap)
@@ -98,6 +122,7 @@ def spawn_barrel(state, tilemap, scene, physics_engine):
         barrel,
         friction=barrel.friction,
         collision_type=COLLISION_ENEMY,
+        mass=MASS,
         max_velocity=200,
         damping=barrel.damping
     )
