29b8b9077a23906719b88e1875c4c85b94a8521e
Your Name <you@example.com>
2024-08-30 15:24:46 +0200

    Lava converted to png

--- src/utils/scene.py
+++ src/utils/scene.py
@@ -261,17 +261,3 @@ def animated_in_sight(size, scene, player_sprite) -> list:
                 animated.append(sprite)
 
     return animated
-
-
-def get_layer(name: str, scene: Scene) -> SpriteList:
-    """
-    Get layer from scene
-    @param name: Name of layer
-    @param scene: Scene
-    @return: List of sprites
-    """
-
-    if name in scene.name_mapping:
-        return scene[name]
-
-    return SpriteList(use_spatial_hash=True, lazy=True)
