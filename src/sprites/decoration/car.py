5bbb08c83c6848912b9e00ac176bd5bfc0fb7e3a
Your Name <you@example.com>
2024-09-14 12:19:45 +0200

    Remove food colliding with car

--- src/sprites/decoration/car.py
+++ src/sprites/decoration/car.py
@@ -1,7 +1,6 @@
 import logging
 
 import arcade
-import pyglet
 
 from constants.collisions import COLLISION_CAR, COLLISION_ENEMY, COLLISION_PLAYER, COLLISION_CHICKEN
 from sprites.sprite import Sprite
@@ -15,28 +14,11 @@ HURT = 34
 
 
 class Car:
-    def remove_food(self, delta_time, args):
-        from constants.layers import LAYER_FOOD
-        try:
-            food = args.scene[LAYER_FOOD]
-        except KeyError:
-            return
-
-        food = filter(lambda x: arcade.get_distance_between_sprites(self, x) <= 200, food)
-
-        print(arcade.get_distance_between_sprites(self, args.player))
-
-        for meal in food:
-            logging.info('Car collided with food')
-            meal.remove_from_sprite_lists()
-
 
     def setup_handlers(self, args):
         args.physics_engine.add_collision_handler(COLLISION_CAR, COLLISION_ENEMY, post_handler=self.on_hit)
         args.physics_engine.add_collision_handler(COLLISION_CAR, COLLISION_PLAYER, post_handler=self.on_hit)
         args.physics_engine.add_collision_handler(COLLISION_CAR, COLLISION_CHICKEN, post_handler=self.on_hit)
-
-        pyglet.clock.schedule_interval_soft(self.remove_food, 2, args=args)
 
     def on_hit(self, _car_sprite, _hit_sprite, _arbiter, _space, _data):
         """ Called for bullet/wall collision """
@@ -60,9 +42,19 @@ class CarLeft(Sprite, Car):
         if self.right < 0:
             args.physics_engine.set_position(self, (w - self.width / 2, self.center_y))
 
+        from constants.layers import LAYER_FOOD
 
-    def cleanup(self):
-        pyglet.clock.unschedule(self.remove_food)
+        try:
+            food = args.scene[LAYER_FOOD]
+        except KeyError:
+            return
+
+        food = filter(lambda x: arcade.get_distance_between_sprites(self, x) < 250, food)
+
+        for meal in food:
+            logging.info('Car collided with food')
+            meal.remove_from_sprite_lists()
+
 
 class CarRight(Sprite, Car):
     def setup(self, args):
@@ -78,6 +70,3 @@ class CarRight(Sprite, Car):
         w, h = args.map_size
         if self.right > w:
             args.physics_engine.set_position(self, (0 - self.width / 2, self.center_y))
-
-    def cleanup(self):
-        pyglet.clock.unschedule(self.remove_food)
