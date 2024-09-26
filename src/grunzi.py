dabce02d36a0d2d9d1c60fc4becfbed6bbcf0422
Your Name <you@example.com>
2024-09-27 00:32:49 +0200

    Format Code

--- src/grunzi.py
+++ src/grunzi.py
@@ -20,15 +20,16 @@ def start() -> None:
 
 
 def start_frozen() -> None:
+    """ Starts the game"""
     try:
         StartUp(os.path.dirname(__file__)).main()
     except KeyboardInterrupt:
-        sys.exit(1)
+        sys.exit(0)
     except Exception as e:
         import logging
 
         logging.critical("Fatal exception", exc_info=e)
-        messagebox.showerror('Error', repr(e))
+        # messagebox.showerror('Error', repr(e))
         sys.exit(1)
 
 
@@ -36,7 +37,7 @@ def start_debug() -> None:
     try:
         StartUp(os.path.dirname(__file__)).main()
     except KeyboardInterrupt:
-        sys.exit(1)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
