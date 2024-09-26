e1f86f8bbfb2fa1a20603f9ba9fafd2e1a3a4f30
Your Name <you@example.com>
2024-09-26 16:39:59 +0200

    Performance

--- src/utils/lookuptable/lookuptableentry.py
+++ src/utils/lookuptable/lookuptableentry.py
@@ -1,14 +1,28 @@
 class LookupTableEntry:
     def __init__(self):
-        self.position = (-1, -1)
+        self.position_1 = (-1, -1)
+        self.position_2 = (-1, -1)
 
         self._value = []
 
-    def needs_update(self, player_sprite):
-        return player_sprite.position != self.position
+    def needs_update(self, sprite1, sprite2 = None):
 
-    def set(self, value, player_sprite):
-        self.position = player_sprite.position
+        check1 =  sprite1.position != self.position_1
+
+        if sprite2 is None:
+            return check1
+
+        check2 = sprite2.position != self.position_2
+
+        return check1 or check2
+
+
+    def set(self, value, sprite1, sprite2 = None):
+        self.position_1 = sprite1.position
+
+        if sprite2 is not None:
+            self.position_2 = sprite2.position
+
         self._value = value
 
     def get(self):
--- src/utils/scene.py
+++ src/utils/scene.py
@@ -92,6 +92,7 @@ class Scene(BaseScene):
     def update_animated(self, delta_time, size, scene, player_sprite):
         """ Update animated """
 
+
         # Animate only visible
         if self.lookup_table.animated_in_sight.needs_update(player_sprite):
             self.lookup_table.animated_in_sight.set(animated_in_sight(size, scene, player_sprite), player_sprite)
