0f5c8ec75ec083e5801aa3630c382377570dd406
Uli <ulidots@ulidots.de>
2024-10-05 19:11:56 +0200

    Level Completed

--- src/utils/eastereggs.py
+++ src/utils/eastereggs.py
@@ -5,4 +5,4 @@ def is_halloween() -> bool:
     """ Check if today is Halloween """
 
     today = date.today()
-    return today.month == 10 and today.day == 5
+    return today.month == 10 and today.day == 31
