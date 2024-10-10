71760a66fe2ae4dc10db8416b521f58fa227ff18
Uli <ulidots@ulidots.de>
2024-10-10 20:19:53 +0200

    Kommentare

--- src/utils/lookuptable/lookuptable.py
+++ src/utils/lookuptable/lookuptable.py
@@ -3,6 +3,8 @@ from utils.lookuptable.lookuptableentry import LookupTableEntry
 
 class LookupTable:
     """ Lookup table """
+
     def __init__(self):
         """ Constructor """
+
         self.animated_in_sight = LookupTableEntry()
