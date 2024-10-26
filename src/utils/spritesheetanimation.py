249f01bdcc0d2891d818b8624d1a7d8547492755
Uli <ulidots@ulidots.de>
2024-10-26 22:11:21 +0200

    Walking animation wip

--- src/utils/spritesheetanimation.py
+++ src/utils/spritesheetanimation.py
@@ -1,13 +1,36 @@
+""" Reads a spritesheet from image """
+
 from PIL import Image
 from PIL.Image import Resampling
 
 
 class SpriteSheetReader:
+    """ Reads a spritesheet from image """
+
     def __init__(self, filename):
+        """
+        Constructor
+        @param filename: The image filename
+        """
         self._filename = filename
         self._images = []
 
-    def process(self, size, autocrop=False, resize=None, pil_resample=Resampling.BILINEAR):
+    def process(
+            self,
+            size: float,
+            autocrop: bool = False,
+            resize: tuple | None = None,
+            pil_resample: int = Resampling.BILINEAR) -> list:
+        """
+        Processes a spritesheet
+        @param size: Size of the spritesheet images
+        @param autocrop: Autocrop the images
+        @param resize: Resize all images to this size
+        @param pil_resample: PIL Resampling algorithm
+
+        @return: List of PIL images
+        """
+
         self._images = []
 
         image = Image.open(self._filename).convert('RGBA').crop()
@@ -42,6 +65,10 @@ class SpriteSheetReader:
 
         self._images = list(reversed(self._images))
 
+        return self.images
+
     @property
     def images(self):
+        """ Return list of images """
+
         return self._images
