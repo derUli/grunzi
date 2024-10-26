eb39239dabba74466c3c2bdb133c836ea1c5f638
Uli <ulidots@ulidots.de>
2024-10-26 09:52:08 +0200

    spritesheetanimation class

--- src/utils/spritesheetanimation.py
+++ src/utils/spritesheetanimation.py
@@ -1,32 +1,47 @@
 import os
 
 from PIL import Image
+from PIL.Image import Resampling
 
-class SpriteSheetAnimation:
+
+class SpriteSheetReader:
     def __init__(self, filename):
         self.filename = filename
+        self.images = []
 
+    def process(self, size, autocrop=False, resize=None):
+        self.images = []
 
-    def process(self, size):
         image = Image.open(self.filename).convert('RGBA').crop()
 
         full_w, full_h = image.size
 
         w, h = size
 
-        x = 0
         y = 0
 
-        while x < full_w:
+        while y < full_h:
+            x = 0
 
-            img_area = (x, y, x + w, y + h )
-            cropped = image.crop(img_area)
+            while x < full_w:
+                img_area = (x, y, x + w, y + h)
+                cropped = image.crop(img_area)
 
-            cropped.save('test_' + str(x) +'.png')
+                if autocrop:
+                    cropped = cropped.crop()
 
-            x += w
+                if resize:
+                    cropped = cropped.resize(
+                        resize,
+                        resample=Resampling.BILINEAR
+                    )
+
+                self.images.append(cropped)
+
+                x += w
+
+            y += h
 
 
-
-test = SpriteSheetAnimation(os.path.abspath('../data/images/sprites/char/pig/pig_walk_run.png'))
-test.process((360, 194))
+# test = SpriteSheetReader(os.path.abspath('../data/images/sprites/char/pig/pig_walk_run.png'))
+# test.process(size=(360, 194), resize=(63, 35))
